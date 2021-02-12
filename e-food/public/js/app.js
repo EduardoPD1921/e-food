c901b498b7d18391ddd66ea122be183f3a628ec3
Eduardo <eduardo.gheleno@gmail.com>
2021-02-12 12:03:52 -0300

    Input and button model created with flexbox

--- e-food/public/js/app.js
+++ e-food/public/js/app.js
@@ -51,6 +51,13 @@ var Header = function Header(props) {
         children: ["Nunca foi t\xE3o f\xE1cil pedir ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("span", {
           children: "comida"
         })]
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("h5", {
+        children: "Encontre restaurantes perto de voc\xEA"
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsxs)("div", {
+        id: "search-input",
+        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("input", {
+          type: "text"
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_1__.jsx)("button", {})]
       })]
     })
   });
--- e-food/resources/js/components/Header.js
+++ e-food/resources/js/components/Header.js
@@ -6,6 +6,11 @@ const Header = props => {
             <div id="header-content">
                 <h2>E-Food</h2>
                 <h3>Nunca foi tão fácil pedir <span>comida</span></h3>
+                <h5>Encontre restaurantes perto de você</h5>
+                <div id="search-input">
+                    <input type="text"></input>
+                    <button></button>
+                </div>
             </div>
         </header>
     )
